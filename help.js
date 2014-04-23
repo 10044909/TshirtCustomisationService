72770246701dcc4f47b2a0ec33a7cccb35bdc08d
10044909 <10044909@brookes.ac.uk>
2014-04-23 16:07:55 +0100

    Customiser Site files

---
+++ help.js
@@ -0,0 +1,16 @@
+document.write("<div id=\"help\">");
+document.write("<b>Help</b> <br> <b> <u> FAQs </u> </b> <br>");
+document.write("	<div id=\"qna\">");
+document.write("		-	<b>Q:</b>I cannot see text on my design. <br>");
+document.write("		&nbsp;&nbsp;&nbsp;	<b>A:</b>Ensure text colour is not the same colour as the text <br><br>");
+document.write("		-	<b>Q:</b>I cannot see a tshirt <br>");
+document.write("		&nbsp;&nbsp;	<b>A:</b> Your browser does not support the HTML5 canvas tag. <br><br>");
+document.write("		-	<b>Q:</b>I cannot add items to cart <br>");
+document.write("		&nbsp;&nbsp;&nbsp;	<b>A:</b> Valid input is required - please refer to error message <br><br>");
+document.write("		-	<b>Q:</b>I have tried all of the above - problem still persists. <br>");
+document.write("		&nbsp;&nbsp;&nbsp;	<b>A:</b> Use the white and blue reset button to refresh interface <br><br>");
+document.write("		-	<b>Q:</b> I cannot see any fields for text entry <br>");
+document.write("		&nbsp;&nbsp;&nbsp;	<b>A:</b> All ad-block software must be disabled when using this site. <br>");
+document.write("		-	<b>Q:</b>  <br>");
+document.write("	</div>");
+document.write("</div>");
